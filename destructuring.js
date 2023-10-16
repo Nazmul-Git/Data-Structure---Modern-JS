4964f44c16a4e60499f517609b5fb78756f7a5ac
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-10-17 02:43:56 +0600

    Array nested  destructuring.

--- destructuring.js
+++ destructuring.js
@@ -1,3 +1,5 @@
+
+// destructuring array
 const arr=[1,2,3];
 const a=arr[0];
 const b=arr[1];
@@ -15,7 +17,11 @@ const student={
     name:'sakib',
     id:20,
     language:['c', 'JS' ],
-
+    framework:['react.js', 'express.js'],
+    weWant: function(firstIdx, secondIdx){
+        return [this.framework[firstIdx], this.framework[secondIdx]]
+    }
+    
 }
 let [first, second]=student.language;
 // console.log(first, second);
@@ -29,3 +35,21 @@ console.log(first, second);
 //  destructuring  swap
 [first, second]=[second, first];
 console.log(first, second); 
+
+// receive 2 return values from a function
+const [firstIdx, secondIdx]=student.weWant(1,0);
+console.log(firstIdx, secondIdx);
+
+// Nested destructuring ------------------
+const arr2=[5,4,2,3,[1,9,0,3]];
+const [i,o,p,q,w]=arr2;
+console.log(i,o,p,q,w);
+
+
+
+
+
+
+
+
+// // destructuring object
