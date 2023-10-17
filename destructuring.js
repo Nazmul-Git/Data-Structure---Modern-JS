96b73be04672992f7b3a5f73114ad60a027621e4
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-10-18 03:18:21 +0600

    Object destructuring- default value and pass a obj as a perameter into a object-function.

--- destructuring.js
+++ destructuring.js
@@ -58,6 +58,10 @@ const studentProfile={
     studentName:'sunny',
     intake:[1,2,3,4,5,6,7,8,9,10,11],
     sec:[1,2,3,4,5,6,7,8,9,10],
+    reputation:[
+      {green:{lightGreen:65,green:100}},
+      {red:{lightRed:65,red:100}}
+    ],
     dept:{
        cse:{
         sub:60,
@@ -71,14 +75,28 @@ const studentProfile={
         sub:45,
         credit:79
        }
+    },
+    orderDestructuring: function({myName,address,gpa}){
+       console.log(myName,address,gpa); //sunny   dhaka   5
     }
+
 };
+
+// pass a object into object-function.
+studentProfile.orderDestructuring({
+    myName:'sunny',
+    address:'dhaka',
+    gpa:5.00
+})
+
+// ********  studentProfile have this keys *********.
 const {studentName,intake,dept}=studentProfile;
 
 // console.log(studentName);
 // console.log(intake);
 // console.log(dept);
 
+// ->  also get keys assigning to new variable
 const {
     studentName: name, 
     intake: batchNumber,
@@ -88,14 +106,22 @@ const {
 // console.log(batchNumber);
 // console.log(department);
 
-// default value
-const {sec=[], sec:section=[]}=studentProfile;
-// console.log(sec);
-// console.log(section);
-// Mutating variable
+//  ********* sec key is an array so get array value_ _ *********
+// -> default value. can access this 2 way.
+const {sec=[]}=studentProfile; 
+const {sec:section=[]}=studentProfile; //assign a new variable
+
+// console.log(sec);  //[1,2,3,4,5,6,7,8,9,10]
+// console.log(section);  //[1,2,3,4,5,6,7,8,9,10]
+
+//  
+// -> Mutating variable
 let l=11;
 let m=55;
 let n=5;
 const obj={l:0, m:0, n:0};
 ({l,m,n}=obj);
-console.log(l,m,n); // 0 0 0
+// console.log(l,m,n); // 0 0 0
+
+
+
