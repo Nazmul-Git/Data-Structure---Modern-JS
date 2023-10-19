de0a86aeede7001f3c43c6f3576789c9ac1e913d
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-10-20 02:27:51 +0600

    Object.keys(), Object.values() and arr.entires() method.

--- for-of-loop.js
+++ for-of-loop.js
@@ -4,17 +4,13 @@ const studentProfile = {
   sec: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
   reputation: [
     {
-      green: {
-        lightGreen: 65,
-        green: 100,
-      },
+      light: 65,
+      dark: 100,
     },
     {
-      red: {
-        lightRed: 65,
-        red: 100,
-      },
-    },
+      light: 55,
+      dark: 99,
+    }
   ],
   dept: {
     cse: {
@@ -36,18 +32,48 @@ const studentProfile = {
 };
 
 // for of loop
-const allReputation=studentProfile.reputation;
-for(const rep of allReputation) console.log(rep); // for of loop give only element not index
-// { green: { lightGreen: 65, green: 100 } }
-// { red: { lightRed: 65, red: 100 } }
+const allReputation = studentProfile.reputation;
+for (const rep of allReputation) console.log(rep); // for of loop give only element not index
+// { light: 65, dark: 100 }
+// { light: 55, dark: 99 }
 
+// -----------------------------
+const keys=Object.keys(studentProfile.dept);
+console.log(keys); //[ 'cse', 'eee', 'eng' ]
+
+const values= Object.values(studentProfile.dept)
+console.log(values);
+/*
+[
+  { sub: 60, credit: 120 },
+  { sub: 50, credit: 100 },
+  { sub: 45, credit: 79 }
+]
+*/
+// ---------------------------
 //  using entries() we can get also index number
-for(const rep of allReputation.entries()) console.log(rep);
-// [ 0, { green: { lightGreen: 65, green: 100 } } ]
-// [ 1, { red: { lightRed: 65, red: 100 } } ]
+for (const rep of allReputation.entries()){
+    // console.log(rep);
+} 
+// [ 0, { light: 65, dark: 100 } ]
+// [ 1, { light: 55, dark: 99 } ]
 //  we can see after use entires() , each item inside an array.
 
+// -----------------------------------------
+
 // destructuring rep
-for(const [idx, element] of allReputation.entries()){
-    console.log(`${idx} : ${element}`);
+//  [ key , value ]
+for (const [idx, element] of allReputation.entries()) {
+//   console.log(`${idx} : ${element}`);
+  //  0 : [object Object]
+  //  1 : [object Object]
+//   here output gives [object Object]. ok.. bcz, element is a object
+
+
+}
+// solved
+for (const [idx, {light, dark}] of allReputation.entries()) {
+  console.log(`${idx} : ${light} , ${dark}`);
+  // 0 : 65 , 100
+ //  1 : 55 , 99
 }
