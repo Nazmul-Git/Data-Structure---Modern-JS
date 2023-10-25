e6e5c1559964d974b830f7b4ef675e19b1342231
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-10-26 05:22:09 +0600

    when we use function expression function then this keyword did not work, solution is-use also apply method, function expression name.apply(obj, arguments will contain an array).

--- higher-order-function.js
+++ higher-order-function.js
@@ -27,7 +27,7 @@ transformer("javascript is the best.", oneWord);
 
 // JS use callback function all the time
 const sayHi = () => {
-  console.log("Hi");
+//   console.log("Hi");
   /* output:
 Hi
 Hi
@@ -59,13 +59,13 @@ greetHey("sunny!");
 // in arrow
 const greetArrow=(greeting)=>{
     return (name)=>{
-        console.log(`${greeting} ${name}`);
+        // console.log(`${greeting} ${name}`);
     };
 }
 const sayHello=greetArrow('Hello!...');
 sayHello('good morning.');
 
-// real example----------------------------------------------
+//---------------------------------------------- real example----------------------------------------------
 
 const flight={
     airline:'Bangladesh Airline',
@@ -75,17 +75,60 @@ const flight={
     bookNow(flightNum, name){
         console.log(`${name} booked a seat on ${this.airline} flights ${this.iataCode}${flightNum}.`);
         //output: Sunny booked a seat on Bangladesh Airline flights BA333.
-        this.bookings.push({flightIs:`${this.iataCode} ${this.flightNum}`, name});
+        this.bookings.push({flightIs:`${this.iataCode} ${this.airline}`, name});
     }
 
 };
 flight.bookNow(333,'Sunny');
 flight.bookNow(123,'Siam');
-console.log(flight.bookings);
+// console.log(flight.bookings);
 /** output:
 [
-  { flightIs: 'BA undefined', name: 'Sunny' },
-  { flightIs: 'BA undefined', name: 'Siam' }
+  { flightIs: 'BA Bangladesh Airline', name: 'Sunny' },
+  { flightIs: 'BA Bangladesh Airline', name: 'Siam' }
 ]
+
+Nazmul booked a seat on Joy Bangla flights JB59.
+
  */
 
+
+
+const booked=flight.bookNow;
+// booked(111, 'Ronaldo'); //TypeError: Cannot read properties of undefined
+// here..... TypeError: Cannot read properties of undefined
+// when we use function expression function then this keyword did not work.
+
+
+// 1. solve this issue: call method
+const joyBangla={
+    airline:'Joy Bangla',
+    iataCode:'JB',
+    bookings:[]
+};
+booked.call(joyBangla, 109, 'Nazmul');
+// console.log(joyBangla);
+/**
+{
+  airline: 'Joy Bangla',
+  iataCode: 'JB',
+  bookings: [ { flightIs: 'JB Joy Bangla', name: 'Nazmul' } ]
+}
+ */
+
+// 2. solve this issue: apply method
+
+const flightData=[109, 'Nazmul'];
+booked.apply(joyBangla, flightData);
+console.log(joyBangla);
+
+/**output:
+{
+  airline: 'Joy Bangla',
+  iataCode: 'JB',
+  bookings: [
+    { flightIs: 'JB Joy Bangla', name: 'Nazmul' },
+    { flightIs: 'JB Joy Bangla', name: 'Nazmul' }
+  ]
+}
+ */
