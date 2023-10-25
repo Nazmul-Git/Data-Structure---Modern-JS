543e031a204477f607b8ba20bf846b4c7e52dfc2
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-10-26 03:22:01 +0600

    Learn about function and higher order function.In higher order most important part is callback function.callback function allow to abstracting abstracting means short the code or hide detailing that is important in js.On the top , we can see the example, where transformer function did not concern about oneWord and upperFirstWord function. And this transformer type of functions are called HIGHER ORDER FUNCTION.

--- conver-camelCase.js
+++ conver-camelCase.js
@@ -1,12 +1,14 @@
 document.querySelector('.btn').addEventListener('click', ()=>{
     const text= document.querySelector('.inp').value;
-    console.log(text);
-    const rows=text.split('\n');
-    for(const row of rows){
+    // console.log(text);
+    const rows=text.split(',');
+    for(const [i,row] of rows.entries()){
         const [first , second]=row.toLowerCase().trim().split('_');
-        const output1=`${first}${second[0].toUpperCase()+second.slice(1)}`
-        const output2=`${first}${second.replace(second[0],second[0].toUpperCase())}`
-        console.log(output1);
-        console.log(output2);
+        // const output1=`${first}${second[0].toUpperCase()+second.slice(1)}`;
+        const output2=`${first}${second.replace(second[0],second[0].toUpperCase())}`;
+
+        console.log(`${output2.padEnd(20)}${'*'.repeat(i+1)}`)
+        // console.log(output1);
+        // console.log(output2);
     }
 })
