57ef4078adc1bd37b28ac18688aa5ceea43ddb1a
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-11-09 01:27:49 +0600

    Closure is a internal property of function.

--- closure.js
+++ closure.js
@@ -7,9 +7,10 @@ const secureBooking= function(){
     }
 }
 const bookedBy= secureBooking();
-bookedBy();
-bookedBy();
-bookedBy();
+bookedBy(); //1
+bookedBy(); //2
+bookedBy(); //3
 
 console.dir(bookedBy); //[Function (anonymous)]
-//  here bookedBy is a function bcz inside bookedBy have secureBooking function and this secureBooking function return a new function. So, return function stored in bookedBy variable, thats the reason bookedBy is a function.We can call bookedBy function directly.
+
+//  here bookedBy is a function bcz inside bookedBy have secureBooking function and this secureBooking function return a new function. So, return function stored in bookedBy variable, thats the reason bookedBy is a function.We can call bookedBy function directly. sourceBooking() executed from call stack and return function stored in bookedBy variable.When we call bookedBy function then return function will execute and inside return function scope have access his parents function scope execution-context like.. passengerCount variable.And this accessing system is called closure.
